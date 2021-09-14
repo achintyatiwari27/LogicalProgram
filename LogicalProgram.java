124d916bc3485143c2e6577a2c914f1e82bcebe7
Achintya <achintyatiwari27>
2021-09-14 21:30:25 +0530

    [Added]:Code to check Fibonacci Series

--- LogicalProgram.java
+++
@@ -1,7 +0,0 @@
-package com.BridgeLabz.com.BridgeLabz;
-
-public class LogicalProgram {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Logical Program !");
-    }
-}
