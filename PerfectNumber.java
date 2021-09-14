f53c8a6844e8df872d3f7ee7c6dc33f3efee94ad
Achintya <achintyatiwari27>
2021-09-14 21:45:07 +0530

    [Added]:Code to check Perfect Number

---
+++ PerfectNumber.java
@@ -0,0 +1,24 @@
+package com.BridgeLabz.com.BridgeLabz;
+
+import java.util.Scanner;
+
+class PerfectNumber {
+    public static void main(String[] args) {
+        int number, result = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        number = sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                result += i;
+            }
+        }
+        if (number == result) {
+            System.out.println("The given number is perfect number");
+        } else {
+            System.out.println("The given number is not perfect number");
+        }
+    }
+}
